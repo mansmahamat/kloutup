5e1ccd4ee5236220f667a4a7dc6e9deea8362aa4
mansmahamat <mansour.mahamat.salle@gmail.com>
2022-12-25 12:54:10 +0100

    update

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,12 +1,11 @@
 // import { Box, useStyleConfig} from '@chakra-ui/react';
 
-import { useStyleConfig, chakra, forwardRef } from '@chakra-ui/react';
-import { CustomCardProps } from 'theme/theme';
-const CustomCard = forwardRef<CustomCardProps, 'div'>((props, ref) => {
-	const { size, variant, ...rest } = props;
-	const styles = useStyleConfig('Card', { size, variant });
+import { useStyleConfig, chakra, forwardRef } from "@chakra-ui/react"
+const CustomCard = forwardRef((props, ref) => {
+  const { size, variant, ...rest } = props
+  const styles = useStyleConfig("Card", { size, variant })
 
-	return <chakra.div ref={ref} __css={styles} {...rest} />;
-});
+  return <chakra.div ref={ref} __css={styles} {...rest} />
+})
 
-export default CustomCard;
+export default CustomCard
