5fcdfa37b3ac582a312c18f562922a6ff89bd66c
Mansour Mahamat-salle <58693907+mansmahamat@users.noreply.github.com>
2023-01-09 10:23:19 +0100

    Update Hero.tsx

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -22,7 +22,7 @@ export function Hero() {
       <div className="my-8 flex justify-center lg:mt-0 lg:flex-shrink-0">
         <div className="inline-flex rounded-md shadow">
           <a
-            href="#"
+            href="#joinlist"
             className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
           >
             Join the wait list
